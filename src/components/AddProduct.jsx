22967330f64d3f3315418fa4af4f522c6e2a15ff
DanishOnGit <youcandoit1911@gmail.com>
2021-11-20 10:49:42 +0530

    initial commit:cra and tailwind setup

---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const AddProduct=()=>{
+    return <>
+    <div>
+        <input type="text" placeholder="Add Name" />
+        <input type="text" placeholder="Add Description" />
+        <input type="file" accept="image/*"/>
+    </div>
+    </>
+}
